1da6bf886610dd381f675c98a9eccdb4e564ce34
Robert Breglio <43708172+pwnzor79@users.noreply.github.com>
2023-07-07 13:54:11 -0600

    gameManager

---
+++ gmtk/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public float time;
+    public int health;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if(instace == null)
+        {
+            destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this.gameObject;
+        }
+
+    }
+
+}
